169584b8d643f0b4cdab394567b0514ff6497f92
Thomas Le Hébel <thomas@MacBook-Pro-de-Thomas.local>
2024-01-13 15:51:18 +0100

    env

--- index.js
+++ index.js
@@ -417,15 +417,15 @@ app.get("/", (req, res) => {
 });
 
 // Northflank va nous fournir une variable process.env.PORT
-// if (process.env.PORT) {
-//   app.listen(process.env.PORT, () => {
-//     console.log("Server started");
-//   });
-// } else {
-//   app.listen(3200, () => {
-//     console.log("Server started");
-//   });
-// }
+if (process.env.PORT) {
+  app.listen(process.env.PORT, () => {
+    console.log("Server started");
+  });
+} else {
+  app.listen(3200, () => {
+    console.log("Server started");
+  });
+}
 
 app.listen(process.env.PORT || 3200, () => {
   console.log("Server started");
